d88f02826c90ffbb8b681523306736e0ff15b002
Francois <francois.coche@outlook.it>
2022-11-04 15:35:30 +0100

    header + footer (tailwind element)

--- project/assets/app.js
+++ project/assets/app.js
@@ -6,6 +6,6 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
 import 'tw-elements';
+import './styles/app.css';
 console.log("Webpack Encore is working");
